6d096e120516566b188665d609e202dec4f0aa08
Alvin Legarbes <a.legarbes.554713@umindanao.edu.ph>
2026-04-20 18:26:48 +0800

    Dockerfile/dockerignore

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\EmployeeController;
 
+Route::get('/', function () {
+    return redirect('/products');
+});
+
 Route::get('/products', [ProductController::class, 'index']);
 Route::post('/products', [ProductController::class, 'store']);
 Route::get('/products/{id}/edit', [ProductController::class, 'edit']);
